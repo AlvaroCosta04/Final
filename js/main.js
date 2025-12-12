66b59767d35fb3f020f1656c4b08a487cf1f696b
AlvaroCosta04 <costamilletalvaro@gmail.com>
2025-12-11 23:56:35 -0300

    Menu hamburguesa arreglado final

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@
 const boton = document.querySelector('.hamburguesa');
 const menu = document.querySelector('.menu');
 
-// Cuando alguien hace clic en el botón...
 boton.addEventListener('click', () => {
-    // ...le ponemos o quitamos la clase "activado" al menú
+    
     menu.classList.toggle('activado');
 });
